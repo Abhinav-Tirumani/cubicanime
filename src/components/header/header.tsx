98dab71258d8aec17276f6cd79127ea09e9cec72
Jujutsu God <122545070+jujutsugod@users.noreply.github.com>
2023-01-29 10:26:07 +0530

    Update header.tsx

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -136,9 +136,7 @@ const Header = ({ visit }: any) => {
           </a>
         </Link>
         <div
-          className={
-            "absolute top-[52px] left-0 md:relative md:top-0 w-full xl:block"
-          )}
+          className="absolute top-[52px] left-0 md:relative md:top-0 w-full xl:block"
         >
           <div
             ref={ref}
